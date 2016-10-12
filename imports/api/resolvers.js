b78c0cf3104007770ee1649f1925d6d9c186dff6
Michiel ter Reehorst <michiel@swydo.com>
2016-10-12 16:23:24 +0200

    Load data from swapi.co

--- imports/api/resolvers.js
+++ imports/api/resolvers.js
@@ -1,19 +1,25 @@
-import { People, Likes } from '../collections';
+import rp from 'request-promise-native';
+import { Likes } from '../collections';
+
+const peopleUrl = 'http://swapi.co/api/people/';
 
 export const resolvers = {
-    RootQuery: {
-        people: () => People.find({}).fetch(),
-        person: (root, { id }) => People.findOne(id)
-    },
-    Person: {
-      _id: ({ _id }) => _id,
-      name: ({ name }) => name,
-      likes: ({ _id }) => Likes.find({personId: _id}).count()
-    },
-    Mutation: {
-      likePerson: (root, { id }) => {
-        Likes.insert({ personId: id });
-        return People.findOne(id);
-      }
+  Person: {
+    _id: ({ url }) => url,
+    name: ({ name }) => name,
+    likes: ({ url }) => Likes.find({ personId: url }).count()
+  },
+
+  RootQuery: {
+    person: (root, { id }) => rp({ uri: id, json: true }),
+    people: (root) => rp({ uri: peopleUrl, json: true })
+                        .then(({ results }) => results)
+  },
+
+  Mutation: {
+    likePerson: (root, { id }) => {
+      Likes.insert({ personId: id });
+      return rp({ uri: id, json: true });
     }
+  }
 };
--- imports/api/schema.js
+++ imports/api/schema.js
@@ -1,23 +1,21 @@
 export const typeDefs = [`
-type Person {
-  _id: ID!
-  name: String!
-  likes: Int
-}
+  type Person {
+    _id: ID!
+    name: String!
+    likes: Int
+  }
 
-type RootQuery {
-  people: [Person]
-  person(id: ID!): Person
-}
+  type RootQuery {
+    person(id: ID!): Person
+    people: [Person]
+  }
 
-type Mutation {
-  likePerson (
-    id: ID!
-  ): Person
-}
+  type Mutation {
+    likePerson(id: ID!): Person
+  }
 
-schema {
-  query: RootQuery
-  mutation: Mutation
-}
+  schema {
+    query: RootQuery
+    mutation: Mutation
+  }
 `];
