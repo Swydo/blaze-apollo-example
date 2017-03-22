5e7e4fa5a75cc2ddf4869b10e7709090dae02a7c
Michiel ter Reehorst <michiel@swydo.com>
2017-03-22 20:01:32 +0100

    No longer need dataIdFromObject with latest apollo client

--- imports/apollo-client.js
+++ imports/apollo-client.js
@@ -3,12 +3,4 @@ import { DDPNetworkInterface } from 'meteor/swydo:ddp-apollo';
 
 export const client = new ApolloClient({
   networkInterface: new DDPNetworkInterface(),
-  dataIdFromObject: (result) => {
-    if (result.id && result.__typename) {
-      const dataId = result.__typename + result.id;
-      return dataId;
-    }
-
-    return null;
-  },
 });
